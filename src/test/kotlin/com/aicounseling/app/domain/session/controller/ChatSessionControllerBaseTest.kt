be571eb88beab7dec4e8c7c127416388dcaa4ec2
Mrbaeksang <qortkdgus95@gmail.com>
2025-08-23 08:19:44 +0900

    fix: ChatSessionControllerBaseTest에 누락된 @Autowired import 추가
    - CI 빌드 실패 문제 해결
    - TestConfig 내부 클래스에서 사용하는 @Autowired 어노테이션 import 추가

--- src/test/kotlin/com/aicounseling/app/domain/session/controller/ChatSessionControllerBaseTest.kt
+++ src/test/kotlin/com/aicounseling/app/domain/session/controller/ChatSessionControllerBaseTest.kt
@@ -15,6 +15,7 @@ import io.mockk.coEvery
 import io.mockk.mockk
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.TestConfiguration
